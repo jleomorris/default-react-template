2320a8f2c02505739c1520f68c15e82c8cc7708b
jleomorris <70540466+jleomorris@users.noreply.github.com>
2021-02-07 03:57:32 +0000

    Setup redux, redux dev tools and form demo

--- src/App.js
+++ src/App.js
@@ -1,29 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // Styled components
 import styled from 'styled-components';
+// Redux
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  initListings,
+  addListing,
+  deleteListing,
+} from './redux/listingsReducer';
 // Images
 import demo from './demo.jpg';
 // Components
 import GlobalStyle from './components/GlobalStyle';
 
-const App = () => (
-  <div className="app">
-    <GlobalStyle />
-    <StyledApp>
-      <img src={demo} alt="background" />
-      <h1>Default React</h1>
-      <p>
-        A preconfigured React environment. Babel, Webpack, Styled Components and
-        ES Linting (Airbnb) all set up and ready to go.
-      </p>
-    </StyledApp>
-  </div>
-);
+const App = () => {
+  const dispatch = useDispatch();
+  const listings = useSelector((state) => state.listings);
+
+  useEffect(() => {
+    dispatch(initListings());
+  }, [dispatch]);
+
+  const removeListing = (listingName) => {
+    dispatch(deleteListing(listingName));
+  };
+
+  return (
+    <div className="app">
+      <GlobalStyle />
+      <StyledApp>
+        <img src={demo} alt="background" />
+        <h1>Preconfigured React</h1>
+        <p>
+          A preconfigured React environment. Babel, Webpack, Styled Components,
+          ES Linting (Airbnb), Redux and Redux Dev Tools all set up and ready to
+          go. A simple form is setup to demo Redux.
+        </p>
+        <StyledTable>
+          <h2>Demo Redux data</h2>
+          <table>
+            {listings.map((listing) => (
+              <tr>
+                <td>{listing.name}</td>
+                <td>{listing.age}</td>
+                <td>
+                  <span
+                    role="button"
+                    tabIndex={0}
+                    onClick={() => removeListing(listing.name)}
+                    onKeyDown={() => {}}
+                  >
+                    (X)
+                  </span>
+                </td>
+              </tr>
+            ))}
+          </table>
+        </StyledTable>
+        <SubmitListing />
+      </StyledApp>
+    </div>
+  );
+};
 
 // Styled components
 const StyledApp = styled.div`
   position: relative;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100vh;
@@ -61,7 +105,50 @@ const StyledApp = styled.div`
     border-radius: 2rem;
     color: white;
     padding: 6rem;
+    margin: 2rem;
   }
 `;
+
+const StyledTable = styled.div`
+  table {
+    color: white;
+    background: rgba(0, 0, 0, 0.2);
+    font-size: 2rem;
+  }
+
+  td {
+    border: 1px solid white;
+    padding: 1rem;
+  }
+`;
+
+// Components
+const SubmitListing = () => {
+  const [name, setName] = useState();
+  const [age, setAge] = useState();
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(addListing(name, age));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Name"
+        onChange={({ target }) => setName(target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Age"
+        onChange={({ target }) => setAge(target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
+// Components
 import App from './App';
+// As @babel/polyfill has been deprecated
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
