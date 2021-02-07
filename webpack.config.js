e534c36cd97c20a4acf5612b2f39d66a127327c0
jleomorris <70540466+jleomorris@users.noreply.github.com>
2021-02-07 21:57:38 +0000

    Add speed measure, progress bar and set webpack config mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+
+const smp = new SpeedMeasurePlugin();
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.[hash].js',
@@ -11,6 +16,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
+    new ProgressBarPlugin(),
   ],
   resolve: {
     modules: [__dirname, 'src', 'node_modules'],
